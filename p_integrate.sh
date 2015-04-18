0f17c89f04a03fc1b7d782a09eb367cd8d73d32f
AleksTheAraneus <perz.aleksandra@gmail.com>
2015-04-18 17:53:55 +0200

    fisrt commit

---
+++ p_integrate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## 0. Prepare environment; install perl, git? basg, libtextutils, fastx...
+## 1. fastq to fasta
+## 2. quality filtering -> save plots and stats
+## 3. FragGeneScan?
+## 4. shuffle seqs: perl shuffleSequences
+## 5. create hash table: velveth
+## 6. assemble contigs: meta-velvetg: memory!
+## 7. annotate genes: prokka
